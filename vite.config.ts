c1c167546136bb9e5a3f70ac9c13495bfafd5c41
Vitalij Ryndin <crashmax.dev@gmail.com>
2026-02-05 22:05:47 +0300

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: './',
   resolve: {
     alias: {
       'navigation-router': fileURLToPath(new URL('./src/lib/index', import.meta.url)),
